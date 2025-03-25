2b5072aa28a13afa0c90aa25d5dcd40055fdbce3
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-25 15:29:45 +0000

    updating the background with interactive videos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,11 +8,11 @@ const Hero = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [loadedVideos, setLoadedVideos] = useState(0)
 
-  const totalVideos = 4;
+  const totalVideos = 3;
   const nextVideoRef = useRef(null) // useRef to store the reference to the next video element 
 
   const handleVideoLoad = () => {
-    setLoadedVideos((oldIndex) => oldIndex + 1)
+    setLoadedVideos((prev) => prev + 1)
   }
 
   // calculate the index of the next video, example -> 0 % 4 = 0 +1 = 1 | 1 % 4 = 1 +1 = 2
@@ -20,10 +20,8 @@ const Hero = () => {
 
   const handleMiniVdClick = () => {
     setHasClicked(true)
-
     setCurrentIndex(upcomingVideoIndex)
   }
-
 
   const getVideoSrc = (index) => {
     return `videos/hero-${index}.mp4` 
@@ -39,17 +37,35 @@ const Hero = () => {
               <div onClick={handleMiniVdClick} className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'>
                 <video
                   ref={nextVideoRef}
-                  src={getVideoSrc(currentIndex + 1)}
+                  src={getVideoSrc(upcomingVideoIndex)}
                   loop
                   muted
                   id="current-video"
                   className='size-64 origin-center scale-150 object-cover object-center'
                   onLoadedData={handleVideoLoad}
-
                 />
               </div>
-
             </div>
+
+            <video 
+              ref={nextVideoRef}
+              src={getVideoSrc(currentIndex)}
+              loop
+              muted
+              id="next-video"
+              className='absolute-center invisible absolute z-20 size-64 object-cover object-center'
+              onLoadedData={handleVideoLoad}
+            />
+
+            <video 
+              src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+              autoPlay
+              loop
+              muted
+              className='absolute left-0 top-0 size-full object-cover object-center'
+            >
+            
+            </video>
          </div>
 
       </div>
