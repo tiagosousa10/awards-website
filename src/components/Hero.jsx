d4afb3688c10f5c34e7caa3b15216248430c7cb2
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-25 15:46:06 +0000

    Button component added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import React, { useRef, useState } from 'react'
+import Button from './Button'
+import { TiLocationArrow } from 'react-icons/ti'
 
 // ----- main function -----
 const Hero = () => {
@@ -32,43 +34,68 @@ const Hero = () => {
     <div className='relative h-dvh w-screen overflow-x-hidden'>
       <div id="video-frame" className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
 
-         <div>
-            <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
-              <div onClick={handleMiniVdClick} className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'>
-                <video
-                  ref={nextVideoRef}
-                  src={getVideoSrc(upcomingVideoIndex)}
-                  loop
-                  muted
-                  id="current-video"
-                  className='size-64 origin-center scale-150 object-cover object-center'
-                  onLoadedData={handleVideoLoad}
-                />
-              </div>
+        <div>
+          <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
+            <div onClick={handleMiniVdClick} className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'>
+              <video
+                ref={nextVideoRef}
+                src={getVideoSrc(upcomingVideoIndex)}
+                loop
+                muted
+                id="current-video"
+                className='size-64 origin-center scale-150 object-cover object-center'
+                onLoadedData={handleVideoLoad}
+              />
             </div>
+          </div>
 
-            <video 
-              ref={nextVideoRef}
-              src={getVideoSrc(currentIndex)}
-              loop
-              muted
-              id="next-video"
-              className='absolute-center invisible absolute z-20 size-64 object-cover object-center'
-              onLoadedData={handleVideoLoad}
+          <video 
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className='absolute-center invisible absolute z-20 size-64 object-cover object-center'
+            onLoadedData={handleVideoLoad}
+          />
+
+          <video 
+            src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+            //autoPlay
+            loop
+            muted
+            className='absolute left-0 top-0 size-full object-cover object-center'
+            onLoadedData={handleVideoLoad}
+          />
+        </div>
+
+        <h1 
+          className='special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75'
+        >
+          G<b>a</b>ming
+        </h1>
+
+        <div className='absolute left-0 top-0 z-40 size-full'>
+          <div className='mt-24 px-5 sm:px-10'>
+            <h1 className='special-font hero-heading text-blue-100'>redefi<b>n</b>e
+            </h1>
+
+            <p 
+              className='mb-5 max-w-64 font-robert-regular text-blue-100'>Enter the Metagame Layer <br/> Unleash the Play Economy
+            </p>
+
+            <Button 
+              id="watch-trailer" 
+              title="Watch Trailer" 
+              leftIcon={<TiLocationArrow />} containerClass="!bg-yellow-300 flex-center gap-1" 
             />
 
-            <video 
-              src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
-              autoPlay
-              loop
-              muted
-              className='absolute left-0 top-0 size-full object-cover object-center'
-            >
-            
-            </video>
-         </div>
+          </div>
+        </div>
 
       </div>
+
+      
     </div>
   )
 }
