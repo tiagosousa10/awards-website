69132dc689182f3742a8cee492760ee47b69c82d
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-26 17:23:20 +0000

    About component section added with gsap animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './index.css'
 import Hero from './components/Hero'
+import About from './components/About'
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
       <Hero />
-
-      <section className='z-0 min-h-screen bg-blue-500'></section>
+      <About />
     </main>
   )
 }
