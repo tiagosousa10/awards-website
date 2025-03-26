bf07e3f81b0d02bb42b886c3b2def5d49858d091
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-26 17:39:20 +0000

    AnimatedTitle added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,7 @@ import gsap from 'gsap'
 import React from 'react'
 
 import { ScrollTrigger } from 'gsap/all'
+import AnimatedTitle from './AnimatedTitle'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -30,10 +31,14 @@ const About = () => {
   return (
     <div id="about" className='min-h-screen w-screen'>
       <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
-         <h2 className='font-general text-sm uppercase md:text-[10px]'>Welcome to Zentry</h2>
-         <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
-            Disc<b>o</b>ver the world's <br/> l<b>a</b>rgest shared adventure
-         </div>
+         <h2 className='font-general text-sm uppercase md:text-[10px]'>
+            Welcome to Zentry
+         </h2>
+
+         <AnimatedTitle 
+            title="Disc<b>o</b>ver the world's <br/> l<b>a</b>rgest shared adventure" 
+            containerClass={"mt-5 !text-black text-center"}   
+         />
 
          <div className='about-subtext'>
             <p>
