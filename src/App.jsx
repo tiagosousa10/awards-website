1461939ef5ae96f546435b96b526cedc63ff481f
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-26 18:09:02 +0000

    audio configurations on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden bg-zinc-600'>
+    <main className='relative min-h-screen w-screen overflow-x-hidden '>
       <Navbar />
       <Hero />
       <About />
