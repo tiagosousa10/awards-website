e5e112794805f0a1b83d11c20ae4af0f176b37b1
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-29 11:05:31 +0000

    gsap animations for story section

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,16 +1,51 @@
 import React, { useRef } from 'react'
 import AnimatedTitle from './AnimatedTitle'
+import gsap from 'gsap'
 
 const Story = () => {
    const frameRef = useRef('null')
-   
+
    const handleMouseLeave = () => {
+      const element = frameRef.current; // Get the frame element
 
+       // Apply the tilt - reset
+       gsap.to(element, {
+         duration: 0.3,
+         rotateX: 0,
+         rotateY: 0,
+         ease: "power1.inOut"
+      })
    }
 
-   const handleMouseMove = () => {
+   const handleMouseMove = (e) => {
+      const {clientX, clientY} = e;
+      const element = frameRef.current; // Get the frame element
 
+      if(!element) return;
+
+      // Get the position of the element
+      const rect = element.getBoundingClientRect();
+      // Get the position of the mouse
+      const x = clientX - rect.left;
+      const y = clientY - rect.top;
+
+      // Get the center of the element
+      const centerX = rect.width / 2;
+      const centerY = rect.height / 2;
+
+      // Calculate the tilt
+      const rotateX = ((y- centerY) / centerY) * -10;
+      const rotateY = ((x- centerX) / centerX) * 10;
+
+      // Apply the tilt
+      gsap.to(element, {
+         duration: 0.3,
+         rotateX, rotateY,
+         transformPerspective: 500,
+         ease: "power1.inOut"
+      })
    }
+
 
   return (
     <section id="story" className='min-h-dvh w-screen bg-black text-blue-50'>
