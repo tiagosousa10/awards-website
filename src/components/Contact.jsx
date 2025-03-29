af84422742998ac991b830a002a858c1aca97fdf
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-29 11:49:05 +0000

    contact section completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from './Button'
 
 const ImageClipBox = ({src, clipClass}) => (
    <div className={clipClass}>
@@ -21,6 +22,33 @@ const Contact = () => {
                src="/img/contact-1.webp"
             />  
 
+         </div>
+
+         <div className='absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80 '>
+            <ImageClipBox 
+                  src="/img/swordman-partial.webp"
+                  clipClass="absolute md:scale-125"
+            />  
+
+            <ImageClipBox 
+               src="/img/swordman.webp"
+               clipClass="sword-man-clip-path md:scale-125"
+            />  
+         </div>
+
+         <div className='flex flex-col items-center text-center'>
+            <p className='font-general text-[10px] uppercase'>
+               Join Zentry
+            </p>
+
+            <p className='special-font mt-10 w-full font-zentry text-5xl leading-[0.9] md:text-[6rem]'>
+               Let's b<b>u</b>ild the <br /> new era of <br /> g<b>a</b>ming t<b>o</b>gether
+            </p>
+
+            <Button 
+               title="contact us"
+               containerClass="mt-10 cursor-pointer"
+            />
          </div>
 
       </div>
