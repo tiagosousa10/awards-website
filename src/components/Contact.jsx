126ce6ae8203d4064836f2844af43fb00eb653ed
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-29 11:36:52 +0000

    first steps in contact section, imageclipbox added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
+const ImageClipBox = ({src, clipClass}) => (
+   <div className={clipClass}>
+      <img src={src} alt="image-clip-box" />
+   </div>
+)
+
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div id="contact" className='my-20 min-h-96 w-screen px-10'>
+      <div className='relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden'>
+         <div className='absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96'>
+            <ImageClipBox 
+               clipClass="contact-clip-path-1"
+               src="/img/contact-1.webp"
+            />  
+
+            <ImageClipBox 
+               clipClass="contact-clip-path-2 lg:translate-y-40 translate-y-60"
+               src="/img/contact-1.webp"
+            />  
+
+         </div>
+
+      </div>
+    </div>
   )
 }
 
